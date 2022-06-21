b3e2feaa234cd0fba9d85e08d2de6de99c8c153e
JeanSolo <jeanpksolo@gmail.com>
2022-06-20 21:47:18 -0400

    feat: fetch initial database data

--- web/src/components/Dashboard.js
+++ web/src/components/Dashboard.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axiosInstance from "../utils/axios";
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axiosInstance.get("/api/v1/users");
+    setData(response.data.results);
+  };
+
   return (
     <>
-      <div>Dashboard</div>;
+      <div>Dashboard</div>
+      {data.map((user, index) => (
+        <div key={index} className="userInfo">
+          <p>Email: {user.email}</p>
+          <p>Role {user.role}</p>
+        </div>
+      ))}
     </>
   );
 };
