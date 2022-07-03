a93ba52f18ce605e8f9001403013942a9585f5af
JeanSolo <jeanpksolo@gmail.com>
2022-07-03 14:38:10 -0400

    refactor: remove logo icon and add cart icon for buyer dashboard

--- web/src/components/ResponsiveAppBar.js
+++ web/src/components/ResponsiveAppBar.js
@@ -84,50 +84,40 @@ const ResponsiveAppBar = () => {
 
           {user.role === "BUYER" && (
             <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
+              <Box onClick={() => navigate("/cart")}>
+                <ShoppingCartIcon />
+              </Box>
+              <Box
                 sx={{
-                  display: { xs: "block", md: "none" },
+                  backgroundColor: "red",
+                  borderRadius: "50%",
+                  marginLeft: 0.5,
+                  borderColor: "error.main",
+                  width: 30,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
               >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+                <Typography
+                  sx={{
+                    fontWeight: "600",
+                    fontSize: 16,
+                  }}
+                >
+                  {products ? products.length : 0}
+                </Typography>
+              </Box>
             </Box>
           )}
-          <StoreIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+
           <Typography
             variant="h5"
             noWrap
             component="a"
             href="/"
             sx={{
-              mr: 2,
+              mr: 1,
               display: { xs: "flex", md: "none" },
               flexGrow: 1,
               fontFamily: "monospace",
